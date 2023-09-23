112fd5a33c68f68f7ab2a3da05f2ef8aa5fdbc40
Chen Si <chensi2024@u.northwestern.edu>
2023-09-22 19:45:27 -0500

    Fetch Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Banner from "./Components/Banner";
 import CourseList from "./Components/CourseList";
 import "./App.css";
 
-const schedule = {
-  title: "CS Courses for 2018-2019",
-  courses: {
-    F101: {
-      term: "Fall",
-      number: "101",
-      meets: "MWF 11:00-11:50",
-      title: "Computer Science: Concepts, Philosophy, and Connections",
-    },
-    F110: {
-      term: "Fall",
-      number: "110",
-      meets: "MWF 10:00-10:50",
-      title: "Intro Programming for non-majors",
-    },
-    S313: {
-      term: "Spring",
-      number: "313",
-      meets: "TuTh 15:30-16:50",
-      title: "Tangible Interaction Design and Learning",
-    },
-    S314: {
-      term: "Spring",
-      number: "314",
-      meets: "TuTh 9:30-10:50",
-      title: "Tech & Human Interaction",
-    },
-  },
-};
-
 const App = () => {
-  const [count, setCount] = useState(0);
-  // console.log(schedule.courses);
+  const [schedule, setSchedule] = useState({});
+  const fetchUrl =
+    "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php";
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(fetchUrl);
+      const data = await response.json();
+      setSchedule(data);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <h1>Wait for Loading</h1>;
+  }
 
   return (
     <div className="App">
